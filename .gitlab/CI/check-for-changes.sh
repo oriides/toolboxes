ee0715279620b37d54b837c56e03e9154b46ac11
Nikolai Thees <nikolaithees@gmail.com>
2023-08-11 16:44:34 +0200

    fixed check-for-changes script

--- .gitlab/CI/check-for-changes.sh
+++ .gitlab/CI/check-for-changes.sh
@@ -7,7 +7,7 @@ touch changed_images
 for folder in *; do
     echo "checking $folder for changes"
 
-    if git diff --name-only $CI_COMMIT_BEFORE_SHA..$CI_COMMIT_SHA | grep -q "^$folder/"; then
+    if git diff $CI_COMMIT_BEFORE_SHA..$CI_COMMIT_SHA | grep "images/$folder/"; then
         echo "Folder contents have changed. Running script...";
         
         echo "$folder" >> ./changed_images
