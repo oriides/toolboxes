9c5a67fb2d4312b002ba782d6bfa415df60e8583
Nikolai Thees <nikolaithees@gmail.com>
2023-08-11 22:24:45 +0200

    added finally job so downstream pipeline is not empty

---
+++ .gitlab/CI/create-job-definition.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat >> .gitlab/CI/dynamic-gitlab-ci.yml <<- EOM
+
+build:$IMAGE:
+  stage: build
+  variables:
+    IMAGE: $IMAGE
+  script:
+    - docker build -t $CI_REGISTRY/oriides/toolboxes/$IMAGE:$CI_COMMIT_SHORT_SHA -t $CI_REGISTRY/oriides/toolboxes/$IMAGE:latest ./$IMAGE/
+    - docker push --all-tags $CI_REGISTRY/oriides/toolboxes/$IMAGE
+EOM
--- .gitlab/CI/create-jobs.sh
+++ .gitlab/CI/create-jobs.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-cat >> .gitlab/CI/dynamic-gitlab-ci.yml <<- EOM
+file="images/changed_images"
 
-build:$IMAGE:
-  stage: build
-  variables:
-    IMAGE: $IMAGE
-  script:
-    - docker build -t $CI_REGISTRY/oriides/toolboxes/$IMAGE:$CI_COMMIT_SHORT_SHA -t $CI_REGISTRY/oriides/toolboxes/$IMAGE:latest ./$IMAGE/
-    - docker push --all-tags $CI_REGISTRY/oriides/toolboxes/$IMAGE
-EOM
+# Check if the file exists
+if [ -f "$file" ]; then
+    echo "creating build jobs for modified images..."
+
+    # Read and print each line
+    while IFS= read -r line; do
+        echo "creating build job for \"$line\" image"
+
+        IMAGE=$line .gitlab/CI/create-job-definition.sh
+
+    done < "$file"
+else
+    echo "File not found: $file"
+    echo "This file is supposed to be provided by previous job check-for-changes, did it run successfully and provide the artifact?"
+    exit 1
+fi
