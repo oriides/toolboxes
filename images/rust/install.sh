55b77103188c08228e6e375cc0eb65a1a34f894a
Nikolai Thees <nikolaithees@gmail.com>
2023-08-12 19:04:17 +0200

    installing the cargo utilities as part of the image is too complicated

---
+++ images/rust/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cargo install cargo-update
+cargo install cargo-audit
+cargo install cargo-outdated
+cargo install cargo-tarpaulin
+cargo install cargo-geiger
+
+rm -rf $HOME/.cargo/registry/{cache,src}
