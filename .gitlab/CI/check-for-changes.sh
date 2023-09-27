ee80273ed9cb0dfc430a1d130cfc848ea5f5e620
Nikolai Thees <nikolaithees@gmail.com>
2023-09-27 14:55:49 +0200

    minor fix for check-for-changes script

--- .gitlab/CI/check-for-changes.sh
+++ .gitlab/CI/check-for-changes.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-cd images
-
 touch changed_images
+
+cd images
 
 # Check if the UPDATE_ALL environment variable is set to true.
 if [[ "${UPDATE_ALL:-}" == "true" ]]; then
   echo "UPDATE_ALL environment variable is set to true. Updating all images…"
 
   for folder in *; do
-    echo "$folder" >> ./changed_images
+    echo "$folder" >> ../changed_images
   done
 
 else
@@ -20,7 +20,7 @@ else
     if git diff $CI_COMMIT_BEFORE_SHA..$CI_COMMIT_SHA | grep "images/$folder/"; then
         echo "Folder contents have changed. Running script…";
         
-        echo "$folder" >> ./changed_images
+        echo "$folder" >> ../changed_images
 
       else
         echo "No changes in the folder. Skipping…";
