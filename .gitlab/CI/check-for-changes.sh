c8a7b4a074ef92aedf86e23280926ba4c187a7cc
Nikolai Thees <nikolaithees@gmail.com>
2023-09-27 14:49:19 +0200

    update check-for-changes script add minikube to k8s toolbox

--- .gitlab/CI/check-for-changes.sh
+++ .gitlab/CI/check-for-changes.sh
@@ -4,15 +4,27 @@ cd images
 
 touch changed_images
 
-for folder in *; do
+# Check if the UPDATE_ALL environment variable is set to true.
+if [[ "${UPDATE_ALL:-}" == "true" ]]; then
+  echo "UPDATE_ALL environment variable is set to true. Updating all images…"
+
+  for folder in *; do
+    echo "$folder" >> ./changed_images
+  done
+
+else
+
+  for folder in *; do
     echo "checking $folder for changes"
 
     if git diff $CI_COMMIT_BEFORE_SHA..$CI_COMMIT_SHA | grep "images/$folder/"; then
-        echo "Folder contents have changed. Running script...";
+        echo "Folder contents have changed. Running script…";
         
         echo "$folder" >> ./changed_images
 
       else
-        echo "No changes in the folder. Skipping...";
+        echo "No changes in the folder. Skipping…";
       fi
-done
+  done
+
+fi
--- images/k8s/README.md
+++ images/k8s/README.md
@@ -2,6 +2,8 @@
 
 K8s management environment for [Toolbox](containertoolbx.org/)
 
+Also ships [minikube](https://minikube.sigs.k8s.io/docs/start/) for local k8s development.
+
 ## Usage
 
 after you created the toolbox with this image, you can enter the environment by running
